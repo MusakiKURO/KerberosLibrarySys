e63f8240f36468cf087f4845652b9526489af068
Nisky <3245931880@qq.com>
2022-05-21 20:06:57 +0800

    demo_1

--- Client/demo_MainWindow_logic.py
+++ Client/demo_MainWindow_logic.py
@@ -5,16 +5,43 @@
 from PyQt5 import QtWidgets
 from KerberosLibrarySys.Client import demo_reader_MainWindow
 import socket
-import threading
 import sys
+
+# 要连接的目标IP和Port
+# AS
+AS_IP = ''
+AS_Port = None
+# TGS
+TGS_IP = ''
+TGS_Port = None
+# Server
+Server_IP = ''
+Server_Port = None
 
 
 class MainWindow_Logic(demo_reader_MainWindow.Ui_MainWindow):
     def __init__(self, num):
         super(MainWindow_Logic, self).__init__()
         self.socket = None
-        
-    def Cliet_start(self):
+
+    def Cliet_AS(self):
+        # 创建socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # 取消主动断开连接四次握手后的TIME_WAIT状态
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.connect((AS_IP, AS_Port))
+        try:
+            # 发送数据
+            send_data = " "
+            self.socket.send(send_data.encode('utf-8'))
+
+            # 接收数据
+            recv_data = self.socket.recv(1024)
+
+            print("Message from server: %s" % recv_data.decode('utf-8'))
+            self.socket.close()
+        except socket.error as e:
+            print("Socket error: %s" % str(e))
+            print("Socket error: %s" % str(e))
+        except Exception as e:
+            print("Other exception: %s" % str(e))
+        finally:
+            self.socket.close()
